4ff15cca1e577b64949bd4b06e9832c050d903e8
Marcos Ramirez <maramirez@stp.gov.py>
2017-06-19 23:42:51 -0400

    Resquest por hilo secundario

--- hilos/src/main/java/com/marcos/hilos/MainActivity.java
+++ hilos/src/main/java/com/marcos/hilos/MainActivity.java
@@ -5,29 +5,80 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private EditText editText;
+    private SegundoPlano segundoPlano;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText)findViewById(R.id.editText);
         button =(Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                segundoPlano = new SegundoPlano();
+                segundoPlano.execute();
             }
         });
     }
 
-    private class segundoPlano extends AsyncTask<Void,Void,Void>{
+    private class SegundoPlano extends AsyncTask<Void,Void,String>{
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            return null;
+        protected String doInBackground(Void... voids) {
+            String result = "";
+            try {
+                URL url = new URL("http://www.android.com/");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                result = parseResponse(in);
+                urlConnection.disconnect();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            editText.setText(s);
+        }
+
+        private String parseResponse(InputStream in){
+            StringBuilder sb = new StringBuilder();;
+            try {
+                BufferedReader reader=new BufferedReader(new InputStreamReader(in,"iso-8859-1"),8);
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                in.close();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return sb.toString();
         }
     }
 }
